46f3f6ce0b4e03f252acafe2a66d47b4891bd78c
m15h4l <108218996+m15h4l@users.noreply.github.com>
2022-07-08 16:28:59 +0200

    Update ksa-gsa-wi-bind.sh

--- gcp/ksa-gsa-wi-bind.sh
+++ gcp/ksa-gsa-wi-bind.sh
@@ -1,4 +1,9 @@
-KSA=$1      # kubernetes service account name
-GSA=$2      # google service account name
-PROJECT=$3  # google project id
+KSA=$1          # kubernetes service account name
+GSA=$2          # google service account name
+PROJECT=$3      # google project id
+NS=${4-default} # kubernetes service account namespace; Falls back to "default" when not provided!
 kubectl annotate serviceaccount $KSA iam.gke.io/gcp-service-account=$GSA@$PROJECT.iam.gserviceaccount.com
+
+gcloud iam service-accounts add-iam-policy-binding $GSA@$PROJECT.iam.gserviceaccount.com \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:$PROJECT.svc.id.goog[$NS/$KSA]"
