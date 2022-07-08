7edc0450a3e5797639200da0210e586a4e94fff6
m15h4l <108218996+m15h4l@users.noreply.github.com>
2022-07-08 16:05:24 +0200

    Update all-resources-in-namespace.sh

--- gcp/all-resources-in-namespace.sh
+++ gcp/all-resources-in-namespace.sh
@@ -1,3 +1,3 @@
 [ ! -z $(kubectl get ns | grep "$1") ] \
   && kubectl api-resources --verbs=list --namespaced -o name | grep -v events | xargs -n 1 kubectl get --show-kind --ignore-not-found -n $1 --no-headers \
-  || echo "Namespace $1 not found"
+  || echo "Namespace [$1] not found."
