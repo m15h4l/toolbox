dda026977fe45d4788a91f9a43adae4dcd62485c
m15h4l <108218996+m15h4l@users.noreply.github.com>
2022-07-08 17:44:22 +0200

    Delete context.sh

--- gcp/context.sh
+++
@@ -1,6 +0,0 @@
-USER=$(gcloud auth list --filter=status:ACTIVE --format='value(account)')
-echo "User: $USER"
-
-PROJECT=$(gcloud info --format='value(config.project)')
-PROJECT_NUMBER=$(gcloud projects describe onlineshop-development-porta --format='get(projectNumber)')
-echo "Project: $PROJECT ($PROJECT_NUMBER)"
